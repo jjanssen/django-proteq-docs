1eb665627709492b8acd53ce544df5c93dc31462
Janneke Janssen <j.janssen@lukkien.com>
2012-02-09 19:11:07 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 METADATA = dict(
     name='django-proteq-docs',
-    version='1.0',
+    version='1.1',
 
     author='Janneke Janssen',
     author_email='j.janssen@lukkien.com',
